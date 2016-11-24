0a68cd1185aab7db00cc98f22441f5642f1faea6
Riaan Schoeman <adriaan.s.schoeman@gmail.com>
2016-11-24 10:19:00 +0100

    Added changes styling changes.

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express');
+var minify = require('express-minify');
 var app = express();
 
+// compress all responses
+
+app.use(minify({
+    js_match: /javascript/,
+    css_match: /css/,
+    uglifyJS: undefined,
+    cssmin: undefined,
+    cache: false,
+    onerror: undefined,
+}));
+
 app.get('/', function (req, res, next) {
-    res.sendfile('index.html');
+    console.log("Getting home page : " + __dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 /* serves all the static files */
 app.get(/^(.+)$/, function (req, res) {
     console.log('static file request : ' + JSON.stringify(req.params));
-    res.sendfile(__dirname + req.params[0]);
+    res.sendFile(__dirname + req.params[0]);
 });
 
 app.post('/', function (req, res, next) {
